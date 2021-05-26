c738ec67b08740826f6a13ccf566d624f0a86000
John Kayl Thomas <commander@arceus.hsd1.ga.comcast.net>
2021-05-26 11:45:19 -0400

    First Draft of Case Study

--- start_server.js
+++ start_server.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const serveStatic = require("serve-static")
-const path = require("path")
-const {exec} = require("child_process")
-const app = express()
-const port = 8888
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const { exec } = require("child_process");
+const app = express();
+const port = 3000;
 
-app.use(serveStatic(path.join(__dirname)))
+app.use(serveStatic(path.join(__dirname)));
 
 app.listen(port, () => {
-  console.log("Static file server running at http://localhost:" + port + "/\nCTRL + C to shutdown");
+  console.log(
+    "Static file server running at http://localhost:" +
+      port +
+      "/\nCTRL + C to shutdown"
+  );
   exec(`open http://localhost:${port}`);
-})
+});
